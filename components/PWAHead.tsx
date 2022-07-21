a080929c75ecef5f357aff7e3f0a6d6a07284384
Kende Detar <dkende4@gmail.com>
2022-07-21 11:17:25 +0200

    SEO

--- components/PWAHead.tsx
+++ components/PWAHead.tsx
@@ -2,12 +2,22 @@ import Head from "next/head";
 import React from "react";
 
 const PWAHead = () => {
+    const title = "Rezsi kalkulátor"
+    const description = "Mennyi lesz a rezsi augusztustól? Mennyivel fog emelkedni a rezsi ára? Ingyenes online kalkulátor. Számolja ki, hogy mennyibe fog kerülni a rezsi (földgáz és áram) augusztustól!";
+
     return (
         <>
             <Head>
-                <title>Rezsi kalkulátor</title>
-                <meta name="description" content="Mennyi lesz a rezsi augusztustól? Mennyivel fog emelkedni a rezsi ára? Ingyenes online kalkulátor. Számolja ki, hogy mennyibe fog kerülni a rezsi (földgáz és áram) augusztustól!" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                
+                <title>{title}</title>
+                <meta name="description" content={description} />
+
+                <meta property="og:title" content={title} />
+                <meta property="og:site_name" content={title}></meta>
+                <meta property="og:description" content={description} />
+                <meta property="og:url" content="https://rezsi-kalkulator.netlify.app" />
+                <meta property="og:type" content="website" />
 
                 <link rel="apple-touch-icon" sizes="180x180" href="/pwa/apple-touch-icon.png"/>
                 <link rel="icon" type="image/png" sizes="32x32" href="/pwa/favicon-32x32.png"/>
