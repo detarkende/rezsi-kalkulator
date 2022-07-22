a6f0b2eba8f2c77cbf819bb3d1fae3bbb659df41
Kende Detar <dkende4@gmail.com>
2022-07-22 10:16:20 +0200

    eddigi haladas

--- pages/advanced.tsx
+++ pages/advanced.tsx
@@ -17,9 +17,16 @@ function Advanced() {
 	];
 	const [aramTarifa, setAramTarifa] = useState(aramTarifak[0]);
 
+	const elszamolasiModok = [
+		{ id: 1, nev: "Havi", oszto: 12 },
+		{ id: 2, nev: "Éves", oszto: 1 },
+	]
+	const [elszamolasiMod, setElszamolasiMod] = useState(elszamolasiModok[0]);
+
 	return (
 		<>
-            <Row xs={1} sm={2} lg={3}>
+			<Row xs={1} sm={2} lg={3} className="mb-3">
+				{/* Aram tarifa */}
                 <Col>
                     <Form.Group>
                         <Form.Label>Áramtarifa</Form.Label>
@@ -29,8 +36,21 @@ function Advanced() {
                             ))}
                         </Form.Select>
                     </Form.Group>
+				</Col>
+				
+				{/* Elszamolas modja */}
+				<Col>
+                    <Form.Group>
+                        <Form.Label>Elszámolás módja</Form.Label>
+                        <Form.Select onChange={(e) => setElszamolasiMod(elszamolasiModok.find(t => t.id === Number(e.target.value)) || elszamolasiModok[0])}>
+                            { elszamolasiModok.map((mod) => (
+                                <option key={mod.id} value={mod.id}>{mod.nev}</option>
+                            ))}
+                        </Form.Select>
+                    </Form.Group>
                 </Col>
-
+				
+				{/* Gyerekek szama */}
                 <Col>
                     <FormField 
                         type="number"
@@ -39,7 +59,7 @@ function Advanced() {
                         onChange={(e) => setGyerekek(Number(e.target.value))}
                     />
                 </Col>
-            </Row>           
+            </Row>
 
 			<Row xs={1} sm={2} lg={3}>
 				<Fogyasztasok rezsi={rezsi} setRezsi={setRezsi} />
@@ -49,7 +69,7 @@ function Advanced() {
 				<code>{JSON.stringify(rezsi, null, "\t")}</code>
 			</pre>
             <pre className="mt-5">
-				<code>{JSON.stringify(aramTarifa, null, "\t")}</code>
+				<code>{JSON.stringify(gyerekek, null, "\t")}</code>
 			</pre>
 		</>
 	);
